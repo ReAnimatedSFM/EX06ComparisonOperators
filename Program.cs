25a9146c772ea1073a3ff08b45f8f84880107122
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-11 09:35:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EX06ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 2;
+            int b = 4;
+
+            Console.WriteLine(a == b); //False
+            Console.WriteLine(a>b); //False
+            Console.WriteLine(a <= b); //True
+            Console.WriteLine(a-b > 0); //False
+            Console.WriteLine(a+b > 0); //True
+            Console.WriteLine(a > 1-b); //True
+
+
+            Console.ReadKey();
+        }
+    }
+}
